e4ce5624fa98b6feaffd3377142649fc01f9293f
bwof24 <134535566+bwof24@users.noreply.github.com>
2024-05-21 09:07:02 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
    botToken: "6909520188:AAEBKk6K7YDKuNrx3hZpmepEgnlxzAimkzk",
-   hostURL: "http://ec2-13-229-104-14.ap-southeast-1.compute.amazonaws.com:3000",
+   hostURL: "http://ec2-18-136-207-54.ap-southeast-1.compute.amazonaws.com:3000",
 };
